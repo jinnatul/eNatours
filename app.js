8635ef3bf1e52706ff7466e44a76ab587dc7b1a1
jinnatul <morolswediu@gmail.com>
2020-06-01 18:51:54 +0600

    API handling post request

--- app.js
+++ app.js
@@ -2,13 +2,15 @@ let fs = require('fs');
 let express = require('express');
 let app = express();
 
+app.use(express.json());
+
 let port = 8000;
 
 let tours = JSON.parse( 
   fs.readFileSync(`${__dirname}/resources/assets/data/tours-simple.json`)
 );
 
-app.get('/', (req, res) => {
+app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({
@@ -20,10 +22,21 @@ app.get('/', (req, res) => {
     })
 })
 
-app.post('/', (req, res) => {
-  res
-    .status(200)
-    .send('Data send success')
+app.post('/api/v1/tours', (req, res) => {
+  let newId = tours[tours.length - 1].id + 1;
+  let newTour = Object.assign({id: newId}, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(`${__dirname}/resources/assets/data/tours-simple.json`, 
+    JSON.stringify(tours), (err) => {
+      res.status(201).json({
+        status: 'ok',
+        data: {
+          tour: newTour
+        }
+      })
+    })
 })
 
 
