ef2ca8c52f4449c67fae4fac84fc3a93e1621a58
jinnatul <morolswediu@gmail.com>
2020-06-01 18:59:35 +0600

    API req to Tour details

--- app.js
+++ app.js
@@ -22,6 +22,27 @@ app.get('/api/v1/tours', (req, res) => {
     })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  let id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid request'
+    })
+  }
+
+  let tour = tours.find(el => el.id === id);
+
+  res
+    .status(200)
+    .json({
+      status: "ok",
+      data: {
+        tour
+      }
+    })
+})
+
 app.post('/api/v1/tours', (req, res) => {
   let newId = tours[tours.length - 1].id + 1;
   let newTour = Object.assign({id: newId}, req.body);
