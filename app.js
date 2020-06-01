6122fd0915039968989f327c0a0cbe3362fee48f
jinnatul <morolswediu@gmail.com>
2020-06-01 22:54:31 +0600

    serving static files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let app = express();
 // Middlewares
 app.use(morgan('dev'));
 app.use(express.json());
+app.use(express.static(`${__dirname}/resources/public`));
 app.use((req, res, next) => {
   console.log('Hello from our Middleware');
   next();
