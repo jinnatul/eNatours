8dea6e417ff95afad652fbec02c9a3ef9478fcb7
jinnatul <morolswediu@gmail.com>
2020-06-07 11:52:51 +0600

    data validation in Tour API

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ let tourSchema = new mongoose.Schema({
     type: String,
     required: [true, 'A tour must have a name'],
     unique: true,
-    trim: true
+    trim: true,
+    maxlength: [30, 'A tour name must have less or equal then 30 characters'],
+    minlength: [10, 'A tour name must have more or equal then 10 characters'],
   },
   slug: String,
   duration: {
@@ -19,11 +21,17 @@ let tourSchema = new mongoose.Schema({
   },
   difficulty: {
     type: String,
-    required: [true, 'A tour must have a difficulty']
+    required: [true, 'A tour must have a difficulty'],
+    enum: {
+      values: ['easy', 'medium', 'difficult'],
+      message: 'Difficulty is either: easy, medium, difficult'
+    }
   },
   ratingsAverage: {
    type: Number,
-   default: 4.5
+   default: 4.5,
+   min: [1, 'Rating must be above 1.0'],
+   max: [5, 'Rating must be below 5.0'],
   },
   ratingsQuantity: {
     type: Number,
@@ -33,7 +41,15 @@ let tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a price']
   },
-  priceDiscount: Number,
+  priceDiscount: {
+    type: Number,
+    validate: {
+      validator: function(val) {
+        return val < this.price;
+      },
+      message: 'Discount price ({VALUE}) should be below regular price'
+    }
+  },
   summary: {
     type: String,
     required: [true, 'A tour must have a description'],
