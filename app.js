da9cfe6748744287dd2d1535ee51af1db4523849
jinnatul <morolswediu@gmail.com>
2020-06-09 00:05:15 +0600

    add errorController

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let express = require('express');
 let morgan = require('morgan');
+
+let AppError = require('./utils/appError');
+let globalErrorHandler = require('./controllers/errorController');
 let tourRouter = require('./routes/tourRoutes');
 let userRouter = require('./routes/userRoutes');
 
@@ -26,21 +29,9 @@ app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
-  let err = new Error(`Can't find ${req.originalUrl} on this server!!!`);
-  err.status = 'fail';
-  err.statusCode = 404;
-  
-  next(err);
-})
+  next(new AppError(`Can't find ${req.originalUrl} on this server!!!`, 404));
+});
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
-})
+app.use(globalErrorHandler);
 
 module.exports = app;
