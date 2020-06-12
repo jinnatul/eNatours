941c90cc914a188ae49aeea4d81e61e81e879807
jinnatul <morolswediu@gmail.com>
2020-06-12 17:41:18 +0600

    implementing rate or hits limiting from each IP

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let morgan = require('morgan');
+let rateLimit = require('express-rate-limit');
 
 let AppError = require('./utils/appError');
 let globalErrorHandler = require('./controllers/errorController');
@@ -13,8 +14,18 @@ let app = express();
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+let limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, Please try again in an hour !!!'
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/resources/public`)); // access static content
+
 app.use((req, res, next) => {
   console.log('Hello from our Middleware');
   next();
