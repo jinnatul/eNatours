0711c8794397981c8b65ff66b10f2d4ef1e147e3
jinnatul <morolswediu@gmail.com>
2020-06-14 23:33:20 +0600

    populating reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -31,6 +31,17 @@ let reviewSchema = new mongoose.Schema({
   }
 );
 
+reviewSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'tour',
+    select: 'name'
+  }).populate({
+    path: 'user',
+    select: 'name photo'
+  });
+  next();
+})
+
 let Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
