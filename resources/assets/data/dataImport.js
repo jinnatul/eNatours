5601283fd9da48d8257bfa49a45e831f35359782
jinnatul <morolswediu@gmail.com>
2020-06-15 13:02:51 +0600

    Importing user & review dev data

--- resources/assets/data/dataImport.js
+++ resources/assets/data/dataImport.js
@@ -2,6 +2,8 @@ let fs = require('fs');
 let mongoose = require('mongoose');
 let dotenv = require('dotenv');
 let Tour = require('../../../models/tourModel');
+let User = require('../../../models/userModel');
+let Review = require('../../../models/reviewModel');
 
 dotenv.config({ path: './env/config.env' });
 
@@ -23,11 +25,19 @@ mongoose
 let tours = JSON.parse(
   fs.readFileSync( `${__dirname}/tours.json`, 'utf-8')
 );
+let users = JSON.parse(
+  fs.readFileSync( `${__dirname}/users.json`, 'utf-8')
+);
+let reviews = JSON.parse(
+  fs.readFileSync( `${__dirname}/reviews.json`, 'utf-8')
+);
 
 // Import data into Tour collection
 let importTourData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log('Tour Data Import Successfull');
   } catch (err) {
     console.log(err);
@@ -39,6 +49,8 @@ let importTourData = async () => {
 let deleteTourData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Tour Data Delete Successfull');
   } catch (err) {
     console.log(err);
