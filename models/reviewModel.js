45955d9949b77d7b36a54064f625ca5934f0f646
jinnatul <morolswediu@gmail.com>
2020-06-15 14:47:06 +0600

    preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -32,6 +32,12 @@ let reviewSchema = new mongoose.Schema({
   }
 );
 
+// Preventing Duplicate review each user against each Tour
+reviewSchema.index(
+  { tour: 1, user: 1 }, 
+  { unique: true }
+);
+
 reviewSchema.pre(/^find/, function(next) {
   // this.populate({
   //   path: 'tour',
